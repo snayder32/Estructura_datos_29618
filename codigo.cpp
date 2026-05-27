09eebba0a3cd3a12ac32ace853221f5808b9e24f
Carrizales Sucapuca Snayder R. <61046949@continental.edu.pe>
2026-05-27 16:55:03 -0500

    creando los punteros globales para la pila, lista, cola y el contador de ID

--- codigo.cpp
+++ codigo.cpp
@@ -22,3 +22,20 @@ struct NodoTransaccion {
     string estado;
     NodoTransaccion* siguiente; // Puntero para enlazar con la siguiente transaccion
 };
+// Punteros globales que marcan el punto de partida de nuestras estructuras en la memoria.
+// Se igualan a NULL al inicio para indicarle al programa que las estructuras estan vacias 
+// y no apuntan a ningun lado todavia.
+
+// cimaPila nos sirve para saber cual es el token que esta mas arriba listo para usarse
+NodoAutorizacion* cimaPila = NULL;
+
+// inicioLista nos ayuda a no perder el rastro del primer registro historico que hagamos
+NodoTransaccion* inicioLista = NULL;
+
+// frenteCola y finalCola son necesarios para la cola, uno atiende al primero y el otro inserta al final
+NodoTransaccion* frenteCola = NULL;
+NodoTransaccion* finalCola = NULL;
+
+// Este contador funciona como un generador de tickets. 
+// Empieza en 1000 y cada vez que registremos a un cliente subira a 1001, 1002, etc.
+int contadorID = 1000; 
